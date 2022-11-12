a6f7c41844cbcb86120172bbf7b6970f14a95e93
ElkYTHN <elkinportillo4@gmail.com>
2022-11-12 10:57:47 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO22ELKIN/EJERCICIO22ELKIN/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using EJERCICIO22ELKIN.Controllers;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EJERCICIO22ELKIN
+{
+    public partial class App : Application
+    {
+        static dbvideo basedata;
+
+        public static dbvideo baseda
+        {
+            get
+            {
+
+                if (basedata == null)
+                {
+                    basedata = new dbvideo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "dbvideo.db3"));
+                }
+                return basedata;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ EJERCICIO22ELKIN/EJERCICIO22ELKIN/Models/videorecord.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO22ELKIN.Models
+{
+    public class videorecord
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        public string uri { get; set; }
+
+        public string descripcion { get; set; }
+    }
+}
